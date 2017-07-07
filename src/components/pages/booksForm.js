2d6e290264e3ae77ef06d8d0f7683d92de6a6aa0
zylstraj <colbertlove12@gmail.com>
2017-07-07 12:09:34 -0700

    Updating form
    Making component look a little nicer. Structurally shaping html just a
    little bit more.

--- src/components/pages/booksForm.js
+++ src/components/pages/booksForm.js
@@ -8,12 +8,18 @@ class BooksForm extends React.Component{
     return(
       <div>
       <form>
-      <label htmlFor="title">Title </label>
+      <div>
+      <label htmlFor="title">Title </label><br/>
       <input type="text" id="title" ref="title" placeholder="Enter Title" />
-      <label htmlFor="description">Description </label>
+      </div>
+      <div>
+      <label htmlFor="description">Description </label><br/>
       <input type="text" id="description" ref="description" placeholder="Enter Description" />
-      <label htmlFor="price">Price </label>
+      </div>
+      <div>
+      <label htmlFor="price">Price </label><br/>
       <input type="text" id="price" ref="price" placeholder="Enter Price" />
+      </div>
       <button type="submit">Save Book</button>
       </form>
       </div>
